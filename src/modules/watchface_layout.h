5abc0c5dce928156d9641055b5fc390708a4e600
Manoj Mehta <jhatax@users.noreply.github.com>
2026-08-07 02:36:56 -0700

    Clang formatting

--- src/modules/watchface_layout.h
+++ src/modules/watchface_layout.h
@@ -12,11 +12,13 @@
 #endif
 
 // Architect
-bool layout_watchface_prepare(int16_t face_width, int16_t face_height, WatchfaceSurface* surface);
+bool layout_watchface_prepare(int16_t face_width,
+    int16_t face_height,
+    WatchfaceSurface* surface);
 
 // Stylist: Palette
 void layout_watchface_update_palette(WatchfaceSurfaceStyle* style,
-  SupportedDisplayModes display_mode);
+    SupportedDisplayModes display_mode);
 
 // Stylist: Fonts
 bool layout_watchface_initialize_fonts(FontBook* fontbook);
